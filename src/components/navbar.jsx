d4c1bd2bd8df12203d6def29ce8d59a674f70d6a
Yana Supriatna <yanasupriatna@Yanas-MacBook-Air.local>
2018-12-15 18:53:52 +0700

    stateless function component example for navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,22 +1,20 @@
 import React, { Component } from "react";
-class Navbar extends Component {
-  state = {};
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Total Item :
-          <span className="badge badge-pill badge-secondary m-2">
-            {this.props.totalCounters}
-          </span>
-          Total Qty :
-          <span className="badge badge-pill badge-secondary m-2">
-            {this.props.totalQty}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
+
+const Navbar = props => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Total Item :
+        <span className="badge badge-pill badge-secondary m-2">
+          {props.totalCounters}
+        </span>
+        Total Qty :
+        <span className="badge badge-pill badge-secondary m-2">
+          {props.totalQty}
+        </span>
+      </a>
+    </nav>
+  );
+};
 
 export default Navbar;
