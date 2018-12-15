3216570ec219e035dc674f407a13d4672d102e39
Yana Supriatna <yanasupriatna@Yanas-MacBook-Air.local>
2018-12-15 19:11:51 +0700

    object de-structuring & refactoring variables

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,20 +1,23 @@
 import React, { Component } from "react";
 
-const Navbar = props => {
-  return (
-    <nav className="navbar navbar-light bg-light">
-      <a className="navbar-brand" href="#">
-        Total Item :
-        <span className="badge badge-pill badge-secondary m-2">
-          {props.totalCounters}
-        </span>
-        Total Qty :
-        <span className="badge badge-pill badge-secondary m-2">
-          {props.totalQty}
-        </span>
-      </a>
-    </nav>
-  );
-};
+class Navbar extends Component {
+  state = {};
+  render() {
+    return (
+      <nav className="navbar navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+          Total Item :
+          <span className="badge badge-pill badge-secondary m-2">
+            {this.props.totalCounters}
+          </span>
+          Total Qty :
+          <span className="badge badge-pill badge-secondary m-2">
+            {this.props.totalQty}
+          </span>
+        </a>
+      </nav>
+    );
+  }
+}
 
 export default Navbar;
